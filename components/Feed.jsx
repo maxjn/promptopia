a98d81ae1a04f8ed13f0e87663fbd8c30a692375
max jn <maxjn763@gmail.com>
2023-05-13 10:11:55 +0330

    Feed: - added get prompt api - added prompt cards - added copy functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "@components/PromptCard";
+
+const PromptList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const fetchPrompts = async () => {
+    const res = await fetch("/api/prompt");
+    const data = await res.json();
+
+    setPrompts(data);
+  };
+
+  useEffect(() => {
+    fetchPrompts();
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    setSearchText(e.target.value);
+  };
+
+  const handleTagClick = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for the prompt..."
+          value={searchText}
+          onChange={handleSearch}
+          className="search_input peer"
+          required
+        />
+      </form>
+
+      <PromptList data={prompts} handleTagClick={handleTagClick} />
+    </section>
+  );
 };
 
 export default Feed;
